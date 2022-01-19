0d661c3540a9fddffa31a2117b40ff51d06c302e
SpyKai0808 <70831065+SpyKai0808@users.noreply.github.com>
2022-01-20 08:59:24 +1000

    ...

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { fstat } = require('fs');
-
 function openNav() {
     document.getElementById("sideNav").style.width = "250px";
 }
@@ -8,6 +6,21 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
 
+const sideNav = document.getElementsByClassName("sideNav")
+
 const fs = require('fs')
 
-fs.readdirSync("./sites").filter(file => file.endsWith('.html'))
+const sitesFolders = fs.readdirSync("./sites")
+
+function createNewElement(folderName, path) {
+    var tag = document.createElement("a")
+    var text = document.createTextNode(`${folderName}`)
+    tag.href = `${path}/index.html`
+    tag.appendChild(text)
+    var element = sideNav
+    element.appendChild(tag)
+}
+
+//for (folder in sitesFolders) {
+    createNewElement(folder, './sites')
+//}

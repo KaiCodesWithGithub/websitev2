c2b3aacabd477de2ea9736e384e8b288bed098bd
SpyKai0808 <70831065+SpyKai0808@users.noreply.github.com>
2022-01-19 15:36:00 +1000

    Update emailer.php

--- contact/emailer.php
+++ contact/emailer.php
@@ -1,12 +1,10 @@
 <?php
 
-$message = '<p>hi</p>'
-
 if($_POST["message"]) {
 
     
 
-mail("kai@homehacks.tk", "Here is the subject line", $message, "From: website@homehacks.tk");
+mail("kai@homehacks.tk", "Here is the subject line", '<p>hi</p>', "From: website@homehacks.tk");
 
 
 }

c8c953d423a28c19e7297b8ece96cc46440b57ca
SpyKai0808 <website@homehacks.tk>
2022-01-21 08:54:03 +1000

    404 page

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
 function openNav() {
     document.getElementById("sideNav").style.width = "250px";
 }
@@ -6,21 +9,27 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
 }
 
-const sideNav = document.getElementsByClassName("sideNav")
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+});
 
-const fs = require('fs')
-
-const sitesFolders = fs.readdirSync("./sites")
-
-function createNewElement(folderName, path) {
-    var tag = document.createElement("a")
-    var text = document.createTextNode(`${folderName}`)
-    tag.href = `${path}/index.html`
-    tag.appendChild(text)
-    var element = sideNav
-    element.appendChild(tag)
-}
-
-//for (folder in sitesFolders) {
-    createNewElement(folder, './sites')
-//}
+// const sideNav = document.getElementsByClassName("sideNav")
+//
+// const sitesFolders = fs.readdirSync("./sites")
+//
+// function createNewElement(folderName, path) {
+//     var tag = document.createElement("a")
+//     var text = document.createTextNode(`${folderName}`)
+//     tag.href = `${path}/index.html`
+//     tag.appendChild(text)
+//     var element = sideNav
+//     element.appendChild(tag)
+// }
+//
+// for (folder in sitesFolders) {
+//     createNewElement(folders, './sites')
+// }

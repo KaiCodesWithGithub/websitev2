31a62cc11830ca9a7fa867b9910c4c5d33612698
SpyKai0808 <website@homehacks.tk>
2022-02-07 16:43:28 +1000

    Update main.js

--- sites/Games/Snake/main.js
+++ sites/Games/Snake/main.js
@@ -2,48 +2,70 @@ var squareSize = 30;
 
 var snake = {
     parts: [
-        {x: 4, y: 2},
-        {x: 3, y: 2},
-        {x: 2, y: 2}
+        { x: 4, y: 2 },
+        { x: 3, y: 2 },
+        { x: 2, y: 2 }
     ],
     facing: "E",
-    nextLocation: function() {
+    nextLocation: function () {
         var snakeHead = snake.parts[0];
         var targetX = snakeHead.x;
         var targetY = snakeHead.y;
-        targetY = snake.facing == "N" ? targetY-1 : targetY;
-        targetY = snake.facing == "S" ? targetY+1 : targetY;
-        targetX = snake.facing == "W" ? targetX-1 : targetX;
-        targetX = snake.facing == "E" ? targetX+1 : targetX;
-        return {x: targetX, y: targetY};
+        targetY = snake.facing == "N" ? targetY - 1 : targetY;
+        targetY = snake.facing == "S" ? targetY + 1 : targetY;
+        targetX = snake.facing == "W" ? targetX - 1 : targetX;
+        targetX = snake.facing == "E" ? targetX + 1 : targetX;
+        return { x: targetX, y: targetY };
     },
-    move: function() {
+    move: function () {
         var location = snake.nextLocation();
-        snake.parts.unshift(location);
-        snake.parts.pop();
+        if (game.isEmpty(location)) {
+            snake.parts.unshift(location);
+            snake.parts.pop();
+        };
+        if (game.isWall(location)) {
+            return "gameover"
+        };
     }
 };
 
 var game = {
     board: [
-        "###############",
-        "#             #",
-        "#             #",
-        "#             #",
-        "#    ####     #",
-        "#    ####     #",
-        "#             #",
-        "#             #",
-        "#             #",
-        "###############"
+        "################",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#     ####     #",
+        "#     ####     #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "#              #",
+        "################"
     ],
     tickNumber: 0,
-    tick: function() {
+    tick: function () {
+        window.clearTimeout(game.timer);
         game.tickNumber++;
+        var result = snake.move();
+        if (result == "gameover") {
+            alert("Game over!");
+            return
+        };
         graphics.drawGame();
-        snake.move();
         game.timer = window.setTimeout("game.tick()", 500);
     },
+    isEmpty: function(location) {
+        return game.board[location.y][location.x] == " ";
+    },
+    isWall: function(location) {
+        return game.board[location.y][location.x] == "#";
+    }
 };
 var graphics = {
     canvas: document.getElementById('canvas'),
@@ -63,7 +85,7 @@ var graphics = {
             currentYoffset += graphics.squareSize;
         });
     },
-    drawSnake: function(ctx) {
+    drawSnake: function (ctx) {
         snake.parts.forEach(function drawPart(part) {
             var partXlocation = part.x * graphics.squareSize;
             var partYlocation = part.y * graphics.squareSize;
@@ -71,7 +93,7 @@ var graphics = {
             ctx.fillRect(partXlocation, partYlocation, graphics.squareSize, graphics.squareSize);
         });
     },
-    drawGame: function() {
+    drawGame: function () {
         var ctx = graphics.canvas.getContext("2d");
         ctx.clearRect(0, 0, graphics.canvas.width, graphics.canvas.height)
         graphics.drawboard(ctx);
@@ -80,7 +102,7 @@ var graphics = {
 };
 graphics.drawGame();
 var gameControl = {
-    processInput: function(keyPressed) {
+    processInput: function (keyPressed) {
         var key = keyPressed.key.toLowerCase();
         var targetDirection = snake.facing;
         if (key == "w") targetDirection = "N";
@@ -88,8 +110,9 @@ var gameControl = {
         if (key == "s") targetDirection = "S";
         if (key == "d") targetDirection = "E";
         snake.facing = targetDirection;
+        game.tick();
     },
-    startGame: function() {
+    startGame: function () {
         window.addEventListener("keypress", gameControl.processInput, false)
         game.tick();
     },
